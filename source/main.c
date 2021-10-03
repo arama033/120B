62b0103ebc5676b85a27fcf6957fd1f8eadcf412
Abhinav Ramachandran <arama033@wch136-24.cs.ucr.edu>
2021-10-02 21:33:56 -0700

    finished exercise 1

--- source/main.c
+++ source/main.c
@@ -7,29 +7,30 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-DDRA = 0x00; PORTA=0xFF;   
- DDRB=0xFF;  PORTB=0x00;
-    /* Insert your solution below */
-	unsigned char tmpB =0x00;
-	unsigned char tmpA=0x00; 
-  while(1) {
-	tmpA=PINA & 0x01;
-	if(tmpA == 0x01){
-		tmpB=(tmpB & 0xFC) | 0x01;
-}
-else{
-	tmpB=(tmpB & 0xFC) | 0x02;
-}
-
-
-	PORTB=tmpB;
-
+int main(void) 
+{
+  DDRA=0x00; PORTA=0xFF;
+  DDRB=0xFF; PORTB=0x00;
+  unsigned char tempA = 0x00;
+  unsigned char tempB = 0x00;
+  while (1)
+   {
+	tempA=PINA & 0x01;
+	tempB=PINA & 0x02;
+	if (tempB==0x00 && tempA==0x01) 
+        {
+		PORTB = 0x01;
+	}
+	else
+        {
+		PORTB=0x00;
+	}
     }
-    return 0;
+    return 1;
 }
